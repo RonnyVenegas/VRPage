2129256c63a9bf7efa0e0514204341af49137cd1
Ronny Venegas <ronnyvenegas11@gmail.com>
2020-06-25 00:38:33 -0600

    dependencies update

--- Velvet-Room-Page/src/app/app-routing.module.ts
+++ Velvet-Room-Page/src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MainComponent } from './main/main.component';
+import { PokemonViewComponent } from './pokemon-view/pokemon-view.component';
 
 
 const routes: Routes = [
-  {path: '', component: MainComponent}
+  {path: '', component: MainComponent},
+  {path: 'pokemon-view', component: PokemonViewComponent}
 ];
 
 @NgModule({
--- Velvet-Room-Page/src/app/app.module.ts
+++ Velvet-Room-Page/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { PokemonViewComponent } from './pokemon-view/pokemon-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
     FooterComponent,
-    NavbarComponent
+    NavbarComponent,
+    PokemonViewComponent
   ],
   imports: [
     BrowserModule,
