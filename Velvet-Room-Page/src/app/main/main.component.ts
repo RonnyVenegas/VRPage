c432479b7accfc962582b66aac0e5c16e56de750
Ronny Venegas <ronnyvenegas11@gmail.com>
2020-06-25 20:47:01 -0600

    Video support

--- Velvet-Room-Page/src/app/main/main.component.ts
+++ Velvet-Room-Page/src/app/main/main.component.ts
@@ -30,7 +30,35 @@ export class MainComponent implements OnInit {
   /** Messages */
   notificationMessage = 'Medidas COVID-19: Actualmente sólo está disponible la zona de ventas en el local Gracias por su comprensión';
 
+  YT: any;
+  video: any;
+  player: any;
+  reframed = false;
+
+  isRestricted = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+  init() {
+    // Return if Player is already created
+    if (window[this.YT]) {
+      this.startVideo();
+      return;
+    }
+
+    const tag = document.createElement('script');
+    tag.src = 'https://www.youtube.com/iframe_api';
+    const firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    /* 3. startVideo() will create an <iframe> (and YouTube player) after the API code downloads. */
+    window['onYouTubeIframeAPIReady'] = () => this.startVideo();
+  }
+
+
   ngOnInit(): void {
+
+    /** Video URL after watch?= */
+    this.video = 'W0W00MaCDBY';
+    this.init();
 
     this.productList.push(new Product('MTGCS0', 'Mtg Core Set 2020', 'Booster Box',
      '85000', 'Disponible', 'Magic The Gathering', 'MTGCORESET2020B.jpg'));
@@ -105,4 +133,76 @@ export class MainComponent implements OnInit {
   modalInjection(product$: Product) {
     this.productModelForModal = product$;
   }
+
+  startVideo() {
+    this.reframed = false;
+    this.player = new window['YT'].Player('player', {
+      videoId: this.video,
+      playerVars: {
+        autoplay: 1,
+        modestbranding: 1,
+        controls: 1,
+        disablekb: 1,
+        rel: 0,
+        showinfo: 0,
+        fs: 0,
+        playsinline: 1
+
+      },
+      events: {
+        onStateChange: this.onPlayerStateChange.bind(this),
+        onError: this.onPlayerError.bind(this),
+        onReady: this.onPlayerReady.bind(this),
+      }
+    });
+  }
+
+  /* 4. It will be called when the Video Player is ready */
+  onPlayerReady(event) {
+    if (this.isRestricted) {
+      event.target.mute();
+      event.target.playVideo();
+    } else {
+      event.target.playVideo();
+    }
+  }
+
+  /* 5. API will call this function when Player State changes like PLAYING, PAUSED, ENDED */
+  onPlayerStateChange(event) {
+    console.log(event)
+    switch (event.data) {
+      case window['YT'].PlayerState.PLAYING:
+        if (this.cleanTime() === 0) {
+          console.log('started ' + this.cleanTime());
+        } else {
+          console.log('playing ' + this.cleanTime());
+        }
+        break;
+      case window['YT'].PlayerState.PAUSED:
+        if (this.player.getDuration() - this.player.getCurrentTime() !== 0) {
+          console.log('paused' + ' @ ' + this.cleanTime());
+        }
+        break;
+      case window['YT'].PlayerState.ENDED:
+        console.log('ended ');
+        break;
+    }
+  }
+
+  cleanTime() {
+    return Math.round(this.player.getCurrentTime());
+  }
+
+  onPlayerError(event) {
+    switch (event.data) {
+      case 2:
+        console.log('' + this.video);
+        break;
+      case 100:
+        break;
+      case 101 || 150:
+        break;
+    }
+  }
+
 }
